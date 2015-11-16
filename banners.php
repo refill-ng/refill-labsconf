81c1b20e3aebbc350ed54287107b85b08a25162b
Zhaofeng Li <hello@zhaofeng.li>
2015-11-16 16:54:31 +0800

    banners.php: Warn Microsoft Edge users about the XSS filter

--- banners.php
+++ banners.php
@@ -3,8 +3,8 @@
 // TODO: I18N
 
 // IE warning
-if ( strpos( $_SERVER['HTTP_USER_AGENT'], "MSIE" ) !== false || !empty( $_GET['forceiewarning'] ) ) {
-	$banners[] = "<div class='alert alert-danger'>Using the tool on Internet Explorer is not recommended. You may experience strange character replacements.</div>";
+if ( false !== strpos( $_SERVER['HTTP_USER_AGENT'], "MSIE" ) || false !== strpos( $_SERVER['HTTP_USER_AGENT'], "Edge/12" ) || !empty( $_GET['forceiewarning'] ) ) {
+	$banners[] = "<div class='alert alert-danger'>Use Internet Explorer or Microsoft Edge at your own risk. Due to the XSS filter employed on those browsers, you may experience strange character replacements.</div>";
 }
 
 
