6feb3a052f588fd33ae433adbad61b8ad9b907f3
Zhaofeng Li <hello@zhaofeng.li>
2015-11-16 17:14:22 +0800

    Add experimental support for Afrikaans Wikipedia (af)

--- common.php
+++ common.php
@@ -3,7 +3,7 @@
 
 // Supported wikis
 $config['wikis']['wikipedia']['identifiers'] = array(
-	"en", "sv", "nl", "de", "fr", "war", "ceb", "ru", "it", "es", "vi", "pl", "simple", "zh", "ja", "ro", "bn", "pt"
+	"en", "sv", "nl", "de", "fr", "war", "ceb", "ru", "it", "es", "vi", "pl", "simple", "zh", "ja", "ro", "bn", "pt", "af"
 );
 
 // Maximum execution time, in seconds
