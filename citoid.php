8371f1d06ffde00ee93f405d74c42003ea492a5e
Zhaofeng Li <hello@zhaofeng.li>
2015-12-13 10:26:15 +0800

    citoid.php: Fix LinkHandler config

--- citoid.php
+++ citoid.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/test.php";
 
 // Use CitoidLinkHandler
-$config['linkhandler'] = "Reflinks\LinkHandlers\CitoidLinkHandler";
+$config['linkhandlers'][1] = "CitoidLinkHandler";
 
 // Banner
 $banners[] = "<div class='alert alert-info'>This instance of reFill is powered by <a href='https://mediawiki.org/wiki/Citoid'>Citoid</a>.</div>";
