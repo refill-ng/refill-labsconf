6f108191270f959482c6afb0486ea00cf91fe11d
Zhaofeng Li <hello@zhaofeng.li>
2016-06-27 22:29:24 +0800

    test.php: Oops, it's bhwiki

--- test.php
+++ test.php
@@ -6,7 +6,7 @@ require_once __DIR__ . "/common.php";
 error_reporting( E_ALL );
 
 // Experimental wikis
-$config['wm-projects']['wikipedia']['wikis'][] = "th";
+$config['wm-projects']['wikipedia']['wikis'][] = "bh";
 
 // Commit ID in default edit summary
 $config['summaryextra'] = " (" . substr( file_get_contents( ".git/refs/heads/master" ), 0, 7 ) . ")";
